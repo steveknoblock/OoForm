09e3b83bedb83844694796171db16b2cace52e76
steveknoblock <steve.knoblock@gmail.com>
2014-03-26 11:53:44 -0400

    Create ooform-config.php

---
+++ ooform-config.php
@@ -0,0 +1,37 @@
+<?php
+
+
+/**
+ * OoForm Configuration
+ */
+
+
+/**
+ * Dependencies
+ *
+ */
+
+/**
+ * Base path. This is an example. Please change to suit your installation.
+ */
+//define('OOFORMBASE', '/usr/www/users/cityg/phphelp.com/test/projects/classes/ooForm/classes/');
+define('OOFORMBASE', '');
+
+/**
+ * Load class corresponding to template engine.
+ * Delete or commment/uncomment the engine you desire. You must
+ * use a template engine with ooForm.
+ */
+ 
+define('OOFORM_TEMPLATE_ENGINE', 'bigfoot');
+//define('OOFORM_TEMPLATE_ENGINE', 'smarty');
+
+/**
+ * Specify the location of the class file for the template engine.
+ */
+
+//define('OOFORM_TEMPLATE_ENGINE_PATH', '/usr/www/users/username/example.com/classes/templateengine/');
+define('OOFORM_TEMPLATE_ENGINE_PATH', '/usr/home/cityg/public_html/brandymorecastle/sandbox/BigFoot/bigfoot-class.php');
+
+
+?>
