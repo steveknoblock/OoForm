a4e4b8f63d34bc4dda39fc468bea2fffd5317395
steveknoblock <steve.knoblock@gmail.com>
2014-03-26 11:54:39 -0400

    Create ooform-lang.php

---
+++ ooform-lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * OoForm
+ * Language localization file.
+ * Not yet implemented.
+ */
+
+$_lang = array(
+	'field_required' => '%sPlease enter a value for the %s field.%s',
+	'field_invalid' => '%sYou must enter a correct value for the %s field.%s'
+);
+
+?>
